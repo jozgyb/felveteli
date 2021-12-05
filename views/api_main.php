329c509fa0d0c4748e97fdf58dcc101a389509cd
Bence <jzsab1@gmail.com>
2021-12-05 21:16:37 +0100

    külső rest api bugfix

--- views/api_main.php
+++ views/api_main.php
@@ -1,6 +1,6 @@
 <div class="rendez">
         <form method="post">
-        <input type="submit" class="api" name="gombget" value="Lekérdezés" />
+                <input type="submit" class="api" name="gombget" value="Lekérdezés" />
                 <input type="submit" class="api" name="gombpost" value="Beszúrás" />
 
                 <input type="submit" class="api" name="gombput" value="Módosítás" />
@@ -10,19 +10,19 @@
 </div>
 <?php
 if (isset($_POST['gombget'])) {
-$url = "https://gorest.co.in/public/v1/posts?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
+        $url = "https://gorest.co.in/public/v1/posts?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
 
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$tabla = curl_exec($ch);
-curl_close($ch);
- //print_r(json_decode($tabla, JSON_PRETTY_PRINT));
-         echo "<pre>";
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $tabla = curl_exec($ch);
+        curl_close($ch);
+        //print_r(json_decode($tabla, JSON_PRETTY_PRINT));
+        echo "<pre>";
         print_r(json_decode($tabla, true));
         echo "</pre>";
 
-      //  $get_result = json_decode($tabla, true);
+        //  $get_result = json_decode($tabla, true);
 }
 if (isset($_POST['gombpost'])) {
         $url = "https://gorest.co.in/public/v1/posts?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
@@ -41,25 +41,25 @@ if (isset($_POST['gombpost'])) {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $tabla = curl_exec($ch);
         curl_close($ch);
-      
+
 ?>
         <pre><?php
-                print_r(json_decode($tabla, JSON_PRETTY_PRINT));?>
+                print_r(json_decode($tabla, JSON_PRETTY_PRINT)); ?>
                 
                 Válasz:
                 Megkeresni az ID-t:
-                [id] => 1470
-                https://gorest.co.in/public/v1/users/1470
+                [id] => 1471
+                https://gorest.co.in/public/v1/users/1471
                 A most felvitt erőforrás kiíratása:
                 </pre><?php
- $url = "https://gorest.co.in/public/v1/posts/1470?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
+                        $url = "https://gorest.co.in/public/v1/posts/1471?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
 
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$tabla = curl_exec($ch);
-curl_close($ch);
-print_r(json_decode($tabla, JSON_PRETTY_PRINT));
+                        $ch = curl_init($url);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        $tabla = curl_exec($ch);
+                        curl_close($ch);
+                        print_r(json_decode($tabla, JSON_PRETTY_PRINT));
                         ?>
         <pre><?php
                 print_r(json_decode($tabla, JSON_PRETTY_PRINT));
@@ -71,11 +71,11 @@ print_r(json_decode($tabla, JSON_PRETTY_PRINT));
 
 
                 if (isset($_POST['gombput'])) {
-                        $url = "https://gorest.co.in/public/v1/posts/1470?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
+                        $url = "https://gorest.co.in/public/v1/posts/1471?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
 
 
                         $adatok = array(
-                               
+
                                 "user_id" => "2791",
                                 "title" => "Tanár úr módosítás",
                                 "body" => "A tanár úr ezt a sort módosította"
@@ -93,7 +93,7 @@ print_r(json_decode($tabla, JSON_PRETTY_PRINT));
 
 
                 if (isset($_POST['gombtorles'])) {
-                        $url = "https://gorest.co.in/public/v1/posts/1470?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
+                        $url = "https://gorest.co.in/public/v1/posts/1471?access-token=b0510476761951d379e4f092784758d5ea6b432000616b7f2a2cfe5c23822e4c";
 
 
                         $ch = curl_init($url);
@@ -103,7 +103,7 @@ print_r(json_decode($tabla, JSON_PRETTY_PRINT));
                         curl_close($ch);
                         echo "<pre>";
                         print_r(json_decode($tabla, JSON_PRETTY_PRINT));
-                        print_r("A tanár úr ezt a sort törölte! ID: 1470");
+                        print_r("A tanár úr ezt a sort törölte! ID: 1471");
                         echo "</pre>";
                 }
                         ?>
